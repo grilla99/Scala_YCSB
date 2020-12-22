1c7358d6ac22df385b1d4ac6f491cdb1af813921
grilla001 <a.grill@student.reading.ac.uk>
2020-12-22 11:53:56 +0000

    Minor code changes

--- YCSB_Scala/src/main/scala/redis/RedisClient.scala
+++ YCSB_Scala/src/main/scala/redis/RedisClient.scala
@@ -2,4 +2,18 @@ package redis
 
 class RedisClient {
 
+  val HOST_PROPERTY = "redis.host"
+  val PORT_PROPERTY = "redis.port"
+  val PASSWORD_PROPERTY =  "redis.password"
+  val CLUSTER_PROPERTY = "redis.cluster"
+  val TIMEOUT_PROPERTY = "redis.timeout"
+
+  val INDEX_KEY = "_indices"
+
+//  @throws(classOf[Java])  NEED TO ADD IN TRHOWS A DB Exception
+  def init(): Unit = {
+    
+  }
+
+
 }
